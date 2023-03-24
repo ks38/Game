725a23c76f1115647c1976769e4dbf612d19c1f7
knyazevvb-PC-HOME <ks38@mail.ru>
2023-03-24 14:47:21 +0300

    initial commit

---
+++ js/main.js
@@ -0,0 +1,65 @@
+const swiper1 = new Swiper('.swiper_1', {
+  slidesPerView: 1,
+  slidesPerGroup: 1,
+  direction: 'vertical',
+  spaceBetween: 20,
+  navigation: {
+    nextEl: '.wrap1 .button-next',
+    prevEl: '.wrap1 .button-prev',
+  },
+});
+
+const swiper2 = new Swiper('.swiper_2', {
+  slidesPerView: 1,
+  slidesPerGroup: 1,
+  direction: 'vertical',
+  spaceBetween: 20,
+  navigation: {
+    nextEl: '.wrap2 .button-next',
+    prevEl: '.wrap2 .button-prev',
+  },
+});
+
+const swiper3 = new Swiper('.swiper_3', {
+  slidesPerView: 1,
+  slidesPerGroup: 1,
+  direction: 'vertical',
+  spaceBetween: 20,
+  navigation: {
+    nextEl: '.wrap3 .button-next',
+    prevEl: '.wrap3 .button-prev',
+  },
+});
+
+const swiper4 = new Swiper('.swiper_4', {
+  slidesPerView: 1,
+  slidesPerGroup: 1,
+  direction: 'vertical',
+  spaceBetween: 20,
+  navigation: {
+    nextEl: '.wrap4 .button-next',
+    prevEl: '.wrap4 .button-prev',
+  },
+});
+
+const swiper5 = new Swiper('.swiper_5', {
+  slidesPerView: 1,
+  slidesPerGroup: 1,
+  direction: 'vertical',
+  spaceBetween: 20,
+  navigation: {
+    nextEl: '.wrap5 .button-next',
+    prevEl: '.wrap5 .button-prev',
+  },
+});
+
+const swiper6 = new Swiper('.swiper_6', {
+  slidesPerView: 1,
+  slidesPerGroup: 1,
+  direction: 'vertical',
+  spaceBetween: 20,
+  navigation: {
+    nextEl: '.wrap6 .button-next',
+    prevEl: '.wrap6 .button-prev',
+  },
+});
